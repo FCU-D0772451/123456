8dc9cf0c4fd1ac683cd84028da69292d105e3760
hippo <109public@gmail.com>
2022-06-27 20:42:02 +0800

    test1

---
+++ test.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    return render_template('hello!')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
