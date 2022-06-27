ff2bdbc544e9cb5180c4f2f5b1a8f849c7a1740f
hippo <109public@gmail.com>
2022-06-27 21:23:02 +0800

    push1

--- test.py
+++ test.py
@@ -7,7 +7,10 @@ def login():
     img = "hello!"
     return img
 
-
+@app.route('/123', methods=['GET', 'POST'])
+def login():
+    img = "123"
+    return img
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
